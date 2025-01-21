0baf7726304de6950b3a5c659f31f1c80b1f2355
bap <baptiste.chenin.dev@gmail.com>
2025-01-21 13:45:16 +0100

    End of Unit 5 - Astro API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://sprightly-eclair-6f0be4.netlify.app/",
+  integrations: [preact()]
+});
